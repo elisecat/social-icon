6b8c1da3a79debf974743a3bf49da224396c7fd1
elisecat <saraelissad@gmail.com>
2023-11-14 17:12:29 -0500

    components and icons updated

--- cat-social-icon.js
+++ cat-social-icon.js
@@ -1,14 +1,57 @@
-import { LitElement, html, css } from 'lit-element';
+import {LitElement, html, css } from 'lit-element'
+import { icons } from './icons';
 
-export class SocialIcon extends LitElement {
+/**
+ * 
+ * # <social-icon>
+ * 
+ * Web component to create social icons, based on LitElement.
+ * 
+ * The "icon" property is used to set the social network to display
+ * 
+ */
 
-    static get styles() {
+export class CatSocialIcon extends LitElement {
+
+    static get properties() {
+        return { 
+          icon: { type: String }
+        };
+      }
+    
+      constructor() {
+        super();
+        this.icon = 'facebook';
+      }
+    
+      static get styles() {
         return css`
-        `
-    }
-
-    render() {
-        return html``;
-    }
+          :host[hidden] { display: none; }
+          :host { display: inline-block; }
+          path {
+            fill: var(--cat-social-icon-color, #888);
+          }
+          path[fill="none"] {
+            fill: transparent;
+          }
+          svg {
+            width: var(--cat-social-icon-size, 24px);
+            height: var(--cat-social-icon-size, 24px);
+          }
+          div { display: flex;}
+        `;
+      }
+    
+      render() {
+        return html`
+        <div>
+          ${this._getIcon(this.icon)}
+        </div>
+        `;
+      }
+    
+      _getIcon(icon) {
+        return icons[icon];
+      }
 }
-customElements.define('social-icon', SocialIcon);
+customElements.define('cat-social-icon', CatSocialIcon);
