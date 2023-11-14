6342fc25a20b06ffa1260e709efca3e33ede3636
saraduran7 <sara.duran@csiete.org>
2023-11-14 12:01:00 -0500

    first commit

---
+++ social-icon.js
@@ -0,0 +1,14 @@
+import { LitElement, html, css } from 'lit-element';
+
+export class SocialIcon extends LitElement {
+
+    static get styles() {
+        return css`
+        `
+    }
+
+    render() {
+        return html``;
+    }
+}
+customElements.define('social-icon', SocialIcon);
